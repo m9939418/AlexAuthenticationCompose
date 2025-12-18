2d06d5f14ffa5f0a92efafbff13a9d7fce6df8b6
Alex <m9939418@gmail.com>
2025-12-18 22:42:31 +0800

    Initial commit

---
+++ app/src/main/java/com/alex/yang/authenticationcompose/App.kt
@@ -0,0 +1,12 @@
+package com.alex.yang.authenticationcompose
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by AlexYang on 2025/12/18.
+ *
+ *
+ */
+@HiltAndroidApp
+class App : Application()
